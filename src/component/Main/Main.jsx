f8d6b69eadb480b6ca3823dd5ca79e64f6efef46
htdynger <tipo2008utuber@gmail.com>
2025-07-19 15:38:50 +0500

    test 3

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -165,7 +165,7 @@ const Main = () => {
     const getChats = async () => {
 
         try {
-            const res = await axios.get('/api/v1/1/chats', {
+            const res = await axios.get(`${VITE_API_LEGAI}/v1/1/chats`, {
                 headers: {
                   Authorization: `Bearer ${token}`,
                   Accept: 'application/json',
@@ -199,7 +199,7 @@ const Main = () => {
         try {
             const res = await axios.post(
                 
-                '/api/v1/1/chats', 
+                `${VITE_API_LEGAI}/v1/1/chats`, 
                 {
                     "name": formattedTime,
                     "agent_id": 1,
